92c164f61409f46465312e5397c0e323de3909b4
sonota88 <yosiot8753@gmail.com>
2017-04-11 07:06:53 +0900

    fix: normalize path for Windows

--- src/main/java/sample/Config.java
+++ src/main/java/sample/Config.java
@@ -4,7 +4,12 @@ import static util.Utils.readFile;
 
 import java.io.File;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Config {
 
@@ -32,10 +37,40 @@ public class Config {
     }
 
     public static void setCurrentDir(String dir){
-        currentDir = dir;
+        currentDir = normalizePath(System.getProperty("os.name"), dir);
     }
     public static void setProjectDir(String dir){
-        projectDir = dir;
+        projectDir = normalizePath(System.getProperty("os.name"), dir);
+    }
+
+    /**
+     * "/c/path/to/..." => "C:/path/to/..."
+     */
+    private static String normalizePath(String osName, String path) {
+        if (osName.toLowerCase().startsWith("windows")) {
+            List<String> m = re_match("^/([a-zA-Z])/", path);
+            if (m.isEmpty()) {
+                return path;
+            } else {
+                return m.get(1).toUpperCase() + ":" + path.substring(2);
+            }
+        } else {
+            return path;
+        }
+    }
+
+    private static List<String> re_match(String restr, String target) {
+        Pattern p = Pattern.compile(restr);
+        Matcher m = p.matcher(target);
+        if (m.find()) {
+            List<String> ss = new ArrayList<>();
+            for (int i = 0; i <= m.groupCount(); i++) {
+                ss.add(m.group(i));
+            }
+            return ss;
+        } else {
+            return Collections.emptyList();
+        }
     }
 
     public static void load() {
