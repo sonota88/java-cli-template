9dbdb70e9274134793b09850619164fa820d3fa5
sonota88 <yosiot8753@gmail.com>
2018-06-03 12:29:32 +0900

    Format Utils

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -9,41 +9,41 @@ public class Utils {
 
     private static final String LF = "\n";
 
-    public static void print(Object ... os){
-        for(Object o : os){
+    public static void print(Object... os) {
+        for (Object o : os) {
             System.out.print(o);
         }
     }
 
-    public static void puts(Object ... os){
-        for(Object o : os){
+    public static void puts(Object... os) {
+        for (Object o : os) {
             System.out.print(o + LF);
         }
     }
 
-    public static void puts_e(Object ... os){
-        for(Object o : os){
+    public static void puts_e(Object... os) {
+        for (Object o : os) {
             System.err.print(o + LF);
         }
     }
 
-    public static void putskv(Object k, Object v){
+    public static void putskv(Object k, Object v) {
         putsf("%s (%s)", k, v);
     }
 
-    public static void putskv_e(Object k, Object v){
+    public static void putskv_e(Object k, Object v) {
         putsf_e("%s (%s)", k, v);
     }
 
-    public static void putsf(String format, Object ... args){
+    public static void putsf(String format, Object... args) {
         puts(fmt(format, args));
     }
 
-    public static void putsf_e(String format, Object ... args){
+    public static void putsf_e(String format, Object... args) {
         puts_e(fmt(format, args));
     }
 
-    public static String readFile(String path){
+    public static String readFile(String path) {
         try {
             return FileUtils.readFileToString(new File(path));
         } catch (IOException e) {
@@ -51,7 +51,7 @@ public class Utils {
         }
     }
 
-    public static String fmt(String format, Object ... args){
+    public static String fmt(String format, Object... args) {
         return String.format(format, args);
     }
 
