36d5cb23cfa4d25b41232998552aab4d6566ba29
sonota88 <yosiot8753@gmail.com>
2018-06-15 07:06:57 +0900

    exec  gjtri30yu5478

--- src/main/java/sample/model/command/Split.java
+++ src/main/java/sample/model/command/Split.java
@@ -1,11 +1,43 @@
 package sample.model.command;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Split {
 
     public void exec(String file, int div) {
+        int total = countLines(file);
+        List<Range> ranges = generateRanges(total, div);
+        int i = 0;
+        for (Range range : ranges) {
+            try (
+                    InputStream is = new FileInputStream(new File(file));
+                    Reader r = new InputStreamReader(is, StandardCharsets.UTF_8);
+                    BufferedReader br = new BufferedReader(r);
+            ) {
+                int ln = 0;
+                while (true) {
+                    ln += 1;
+                    String line = br.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    if (range.contains(ln)) {
+                        // write
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     private int countLines(String file) {
         // TODO Auto-generated method stub
