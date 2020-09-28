8775f499c8206b7e1f2cb52b4cf76fa72f6cfc2b
sonota88 <yosiot8753@gmail.com>
2020-09-28 09:02:42 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+print_project_dir() {
+  (
+    cd "$(dirname "$0")"
+    pwd
+  )
+}
+
+CURRENT_DIR="$(pwd)"
+PROJECT_DIR="$(print_project_dir)"
+
+. ${PROJECT_DIR}/common.sh
+
+args="$(_build_args "$@")"
+
+java -jar \
+  ${PROJECT_DIR}/target/sample_a-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  "$args"
