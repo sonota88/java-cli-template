62bfb39506c7e811b360ce4f39a8517fc8b2bf1d
sonota88 <yosiot8753@gmail.com>
2022-03-05 12:23:29 +0900

    Change to JUnit5 style (SplitTest.java)

--- src/test/java/sample/model/command/SplitTest.java
+++ src/test/java/sample/model/command/SplitTest.java
@@ -1,13 +1,12 @@
 package sample.model.command;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import sample.model.command.Split.Range;
 
@@ -15,7 +14,7 @@ public class SplitTest {
 
     Split sut;
 
-    @Before
+    @BeforeEach
     public void setup() {
         sut = new Split();
     }
@@ -24,41 +23,41 @@ public class SplitTest {
     public void test_calcBlockSize_div2() {
         int div = 2;
 
-        assertThat(sut.calcBlockSize(0, div), is(0)); // 0 0
-        assertThat(sut.calcBlockSize(1, div), is(1)); // 1 0
-        assertThat(sut.calcBlockSize(2, div), is(1)); // 1 1
-        assertThat(sut.calcBlockSize(3, div), is(2)); // 2 1
-        assertThat(sut.calcBlockSize(4, div), is(2)); // 2 2
-        assertThat(sut.calcBlockSize(5, div), is(3)); // 3 2
+        assertEquals(0, sut.calcBlockSize(0, div)); // 0 0
+        assertEquals(1, sut.calcBlockSize(1, div)); // 1 0
+        assertEquals(1, sut.calcBlockSize(2, div)); // 1 1
+        assertEquals(2, sut.calcBlockSize(3, div)); // 2 1
+        assertEquals(2, sut.calcBlockSize(4, div)); // 2 2
+        assertEquals(3, sut.calcBlockSize(5, div)); // 3 2
 
-        assertThat(sut.calcBlockSize(1999, div), is(1000));
-        assertThat(sut.calcBlockSize(2000, div), is(1000));
-        assertThat(sut.calcBlockSize(2001, div), is(1001));
+        assertEquals(1000, sut.calcBlockSize(1999, div));
+        assertEquals(1000, sut.calcBlockSize(2000, div));
+        assertEquals(1001, sut.calcBlockSize(2001, div));
     }
 
     @Test
     public void test_calcBlockSize_div3() {
         int div = 3;
 
-        assertThat(sut.calcBlockSize(0, div), is(0)); // 0 0 0
-        assertThat(sut.calcBlockSize(1, div), is(1)); // 1 0 0
-        assertThat(sut.calcBlockSize(2, div), is(1)); // 1 1 0
-        assertThat(sut.calcBlockSize(3, div), is(1)); // 1 1 1
-        assertThat(sut.calcBlockSize(4, div), is(2)); // 2 2 0
-        assertThat(sut.calcBlockSize(5, div), is(2)); // 2 2 1
-        assertThat(sut.calcBlockSize(6, div), is(2)); // 2 2 2
-        assertThat(sut.calcBlockSize(7, div), is(3)); // 3 3 1
+        assertEquals(0, sut.calcBlockSize(0, div)); // 0 0 0
+        assertEquals(1, sut.calcBlockSize(1, div)); // 1 0 0
+        assertEquals(1, sut.calcBlockSize(2, div)); // 1 1 0
+        assertEquals(1, sut.calcBlockSize(3, div)); // 1 1 1
+        assertEquals(2, sut.calcBlockSize(4, div)); // 2 2 0
+        assertEquals(2, sut.calcBlockSize(5, div)); // 2 2 1
+        assertEquals(2, sut.calcBlockSize(6, div)); // 2 2 2
+        assertEquals(3, sut.calcBlockSize(7, div)); // 3 3 1
 
-        assertThat(sut.calcBlockSize(2999, div), is(1000));
-        assertThat(sut.calcBlockSize(3000, div), is(1000));
-        assertThat(sut.calcBlockSize(3001, div), is(1001));
+        assertEquals(1000, sut.calcBlockSize(2999, div));
+        assertEquals(1000, sut.calcBlockSize(3000, div));
+        assertEquals(1001, sut.calcBlockSize(3001, div));
     }
 
     @Test
     public void test_generateRanges_div1() {
         {
             List<Range> rs = sut.generateRanges(0, 1);
-            assertThat(dumpRanges(rs), is("()"));
+            assertEquals("()", dumpRanges(rs));
         }
     }
 
@@ -67,40 +66,40 @@ public class SplitTest {
         int div = 2;
         {
             List<Range> rs = sut.generateRanges(0, div);
-            assertThat(dumpRanges(rs), is("() ()"));
+            assertEquals("() ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(1, div);
-            assertThat(dumpRanges(rs), is("(1 1) ()"));
+            assertEquals("(1 1) ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(2, div);
-            assertThat(dumpRanges(rs), is("(1 1) (2 2)"));
+            assertEquals("(1 1) (2 2)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(3, div);
-            assertThat(dumpRanges(rs), is("(1 2) (3 3)"));
+            assertEquals("(1 2) (3 3)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(4, div);
-            assertThat(dumpRanges(rs), is("(1 2) (3 4)"));
+            assertEquals("(1 2) (3 4)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(5, div);
-            assertThat(dumpRanges(rs), is("(1 3) (4 5)"));
+            assertEquals("(1 3) (4 5)", dumpRanges(rs));
         }
 
         {
             List<Range> rs = sut.generateRanges(1999, div);
-            assertThat(dumpRanges(rs), is("(1 1000) (1001 1999)"));
+            assertEquals("(1 1000) (1001 1999)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(2000, div);
-            assertThat(dumpRanges(rs), is("(1 1000) (1001 2000)"));
+            assertEquals("(1 1000) (1001 2000)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(2001, div);
-            assertThat(dumpRanges(rs), is("(1 1001) (1002 2001)"));
+            assertEquals("(1 1001) (1002 2001)", dumpRanges(rs));
         }
     }
 
@@ -109,48 +108,48 @@ public class SplitTest {
         int div = 3;
         {
             List<Range> rs = sut.generateRanges(0, div);
-            assertThat(dumpRanges(rs), is("() () ()"));
+            assertEquals("() () ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(1, div);
-            assertThat(dumpRanges(rs), is("(1 1) () ()"));
+            assertEquals("(1 1) () ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(2, div);
-            assertThat(dumpRanges(rs), is("(1 1) (2 2) ()"));
+            assertEquals("(1 1) (2 2) ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(3, div);
-            assertThat(dumpRanges(rs), is("(1 1) (2 2) (3 3)"));
+            assertEquals("(1 1) (2 2) (3 3)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(4, div);
-            assertThat(dumpRanges(rs), is("(1 2) (3 4) ()"));
+            assertEquals("(1 2) (3 4) ()", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(5, div);
-            assertThat(dumpRanges(rs), is("(1 2) (3 4) (5 5)"));
+            assertEquals("(1 2) (3 4) (5 5)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(6, div);
-            assertThat(dumpRanges(rs), is("(1 2) (3 4) (5 6)"));
+            assertEquals("(1 2) (3 4) (5 6)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(7, div);
-            assertThat(dumpRanges(rs), is("(1 3) (4 6) (7 7)"));
+            assertEquals("(1 3) (4 6) (7 7)", dumpRanges(rs));
         }
 
         {
             List<Range> rs = sut.generateRanges(2999, div);
-            assertThat(dumpRanges(rs), is("(1 1000) (1001 2000) (2001 2999)"));
+            assertEquals("(1 1000) (1001 2000) (2001 2999)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(3000, div);
-            assertThat(dumpRanges(rs), is("(1 1000) (1001 2000) (2001 3000)"));
+            assertEquals("(1 1000) (1001 2000) (2001 3000)", dumpRanges(rs));
         }
         {
             List<Range> rs = sut.generateRanges(3001, div);
-            assertThat(dumpRanges(rs), is("(1 1001) (1002 2002) (2003 3001)"));
+            assertEquals("(1 1001) (1002 2002) (2003 3001)", dumpRanges(rs));
         }
     }
 
